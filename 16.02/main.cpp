0ae5b4583869458eed319150e18a36b5fbef179b
Kreniker <kitserov.mike@gmail.com>
2026-03-02 18:21:07 +0300

    insert

--- 16.02/main.cpp
+++ 16.02/main.cpp
@@ -4,11 +4,20 @@ template< class T > struct BiList {
   BiList< T >* prev;
 };
 
-template<class T>
-BiList< T >* add(BiList<T>* head, T v) {
-  BiList< T >* newNode = new BiList< T >{v, head, nullptr};
-  if (head) {
-    head->prev = newNode;
+template< class T >
+BiList< T >* add(BiList< T >* h, T v) {
+  BiList< T >* newNode = new BiList< T >{v, h, nullptr};
+  if (h) {
+    h->prev = newNode;
+  }
+  return newNode;
+}
+
+template< class T >
+BiList< T >* insert(BiList< T >* node, T v) {
+  BiList< T >* newNode = new BiList< T >{v, nullptr, node};
+  if (node) {
+    node->next = newNode;
   }
   return newNode;
 }
